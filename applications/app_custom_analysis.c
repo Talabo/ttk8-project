0b2d3383e3cba895860385950f5bb9bce47cf46c
emil_ubuntu_vm <emil.jenssen@maritimerobotics.com>
2021-10-14 10:20:21 +0200

    Added sensor pin reading functionalities, and (almost) output pin for custom example.

--- applications/app_custom_analysis.c
+++ applications/app_custom_analysis.c
@@ -42,28 +42,36 @@
 #include <limits.h> // To define limits of integers for terminal inputs
 
 
+#define SENSOR_VCC            5 // Define the voltage level (High level) of the output from sensor
+
 // Threads
 static THD_FUNCTION(custom_analysis_thread, arg);
 static THD_WORKING_AREA(custom_analysis_thread_wa, 2048); // 2kb stack for this thread
 
 // Function Headers
-void app_custom_register_commands(void);
-void app_custom_unregister_commands(void);
+static void app_custom_register_commands(void);
+static void app_custom_unregister_commands(void);
+static void app_custom_create_analysis_thread();
+static double app_custom_sensor_val_voltage_5V();
+static double app_custom_sensor_val_voltage_3V3();
+
 void terminal_custom_set_thread_prority(int argc, const char **argv);
 void terminal_custom_get_thread_prority(int argc, const char **argv);
 void terminal_custom_update_thread_settings(int argc, const char **argv);
-static void app_custom_create_thread();
 
 // Thread variables
 static volatile bool stop_now = true;
 static volatile bool is_running = false;
+static double custom_hall_sensor_val = 0.0;
+static double custom_hall_sensor_val_voltage = 0.0;
 
 // Variables
 static volatile int custom_thread_priority = NORMALPRIO;
 
 
+
 // Register custom commands for callbacks
-void app_custom_register_commands(void){
+static void app_custom_register_commands(void){
 
 	// Thread Priority
 	terminal_register_command_callback(
@@ -88,11 +96,39 @@ void app_custom_register_commands(void){
 
 
 // Unregister custom commands for callbacks
-void app_custom_unregister_commands(void){
+static void app_custom_unregister_commands(void){
 	terminal_unregister_callback(terminal_custom_get_thread_prority);
 	terminal_unregister_callback(terminal_custom_set_thread_prority);
 	terminal_unregister_callback(terminal_custom_update_thread_settings);
 }
+
+// Create the custom analysis thread
+static void app_custom_create_analysis_thread(){
+	chThdCreateStatic(custom_analysis_thread_wa,
+					sizeof(custom_analysis_thread_wa),
+					custom_thread_priority, custom_analysis_thread, NULL);
+}
+
+
+// Reads hall-effect sensor data
+// (ADC_IND_EXT2 =  HW: VESC 6 MkV, COMM PORT, MISO ADC2 Pin)
+static double app_custom_read_hall_sensor(){
+	return (double)ADC_Value(ADC_IND_EXT2);
+}
+
+// Read hall-effect sensor data in voltage level for 5V logic
+// (Hall-sensor uses 5v source, ADC_IND_EXT2 pin is 5V TOLERANT)
+static double app_custom_sensor_val_voltage_5V(){
+	return (double)ADC_Value[ch] / 4096.0 * SENSOR_VCC;
+}
+
+// Read hall-effect sensor data in voltage level for 3.3V logic
+static double app_custom_sensor_val_voltage_3V3(){
+	// ADC_VOLTS(ch) == (float)ADC_Value[ch] / 4096.0 * V_REG
+	// V_REG = 3.3
+	return (double)ADC_VOLTS(ADC_IND_EXT2);
+}
+
 
 void terminal_custom_set_thread_prority(int argc, const char **argv){
 	if(argc == 2) {
@@ -130,11 +166,7 @@ void terminal_custom_update_thread_settings(int argc, const char **argv){
 	app_set_configuration(appconf);
 }
 
-static void app_custom_create_thread(){
-	chThdCreateStatic(custom_analysis_thread_wa,
-					sizeof(custom_analysis_thread_wa),
-					custom_thread_priority, custom_analysis_thread, NULL);
-}
+
 
 
 
@@ -145,10 +177,22 @@ void app_custom_start(void){
 	commands_printf("Custom propeller controller app started");
 
 	// TO START UP STUFF HERE
+	//palReadPad(GPIOA, )
+	// Pin: XXXX as digital output
+	//palSetPadMode(GPIOA, 6, PAL_MODE_OUTPUT_PUSHPULL | PAL_STM32_OSPEED_HIGHEST);
+	palSetPadMode(GPIOB, 0, PAL_MODE_OUTPUT_PUSHPULL | PAL_STM32_OSPEED_HIGHEST);
+	palSetPadMode(HW_UART_TX_PORT, HW_UART_TX_PIN, PAL_MODE_INPUT_PULLDOWN);
+
+
+
+	// Set the MISO ADC2 pin (PA6) as a analog input
+	// NB! Not to mix with ADC_IND_EXT2 = 7 which is part of the ADC Vector (ADC channel)
+	// Commented out, because already established in hwconf/hw_60.c (HW: VESC 6 MkV)
+	//palSetPadMode(GPIOA, 6, PAL_MODE_INPUT_ANALOG);
 
 	// Start Thread
 	stop_now = false;
-	app_custom_create_thread();
+	app_custom_create_analysis_thread();
 }
 
 // Called when the custom application is stopped.
@@ -195,6 +239,10 @@ static THD_FUNCTION(custom_analysis_thread, arg){
 		timeout_reset(); // Reset timeout if everything is OK.
 
 		// Run the thread logic here //
+
+		custom_hall_sensor_val = app_custom_read_hall_sensor();
+		custom_hall_sensor_val_voltage = app_custom_sensor_val_voltage_5V();
+
 
 
 
